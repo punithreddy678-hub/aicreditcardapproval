cc439e43cd15113882f5162b1c243128666bc953
mohanreddy <punithreddy678@gmail.com>
2026-07-08 10:12:02 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  base: process.env.VITE_BASE_URL || '/punithreddy678-hub/',
+  base: '/aicreditcardapproval/',
   build: {
     outDir: 'dist',
     sourcemap: false,
